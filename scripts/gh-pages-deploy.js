ae0c04e861f0a4a7235da57a4c4aa0c5d7a578b2
mmarifat <mma.rifat66@gmail.com>
2021-10-27 21:08:22 +0600

    New Pipe 2

--- scripts/gh-pages-deploy.js
+++ scripts/gh-pages-deploy.js
@@ -4,21 +4,18 @@ const chalk = require("chalk");
 
 const firstLog = emoji.get("fast_forward") + " " + chalk.yellow("Building...");
 const secondLog = emoji.get("fast_forward") + " " + chalk.yellow("Pushing...");
-const thirdLog =
-  emoji.get("rocket") +
-  " " +
-  chalk.green("Your app successfully deployed") +
-  " " +
-  emoji.get("rocket");
+const thirdLog = emoji.get("rocket") + " " + chalk.green("Your app successfully deployed") + " " + emoji.get("rocket");
 
 (async () => {
   try {
     await execa.command('git checkout --orphan gh-pages')
     console.log(firstLog);
+
     await execa.command('quasar build')
     await execa.command('git --work-tree dist/spa add --all')
     await execa.command('git --work-tree dist/spa commit -m "gh-pages"')
     console.log(secondLog);
+
     await execa.command('git push origin HEAD:gh-pages --force')
     await execa.command('rm -r dist/spa')
     await execa.command('git checkout -f master')
